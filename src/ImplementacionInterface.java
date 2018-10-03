c0bddd110228bea477d330adf6756f2cc522f842
Dann Joel <joelin_001@hotmail.com>
2018-10-03 11:23:10 -0500

    Se crearon las claces Main y se implemento lainterface para comenzar con la funcion de la interface

--- src/ImplementacionInterface.java
+++ src/ImplementacionInterface.java
@@ -1,7 +1,28 @@
 import mx.com.sintelti.desafios.oporx.Solucion;
 
+
 public class ImplementacionInterface implements Solucion {
+
+    int lentgth;
+    String salida;
+    char chars;
+
+    public ImplementacionInterface(){
+        this.lentgth = 0;
+        this.salida = "";
+        chars = 'x';
+    }
     public String convertir(String entrada) {
-        return null;
+        this.lentgth = entrada.length();
+
+        for (char caracter:  entrada.toCharArray()) {
+            //System.out.println(caracter);
+            //this.salida = entrada
+            //
+        }
+         for (int x =0; x<=lentgth; x++){
+           salida = entrada.replace('o',this.chars);
+        }
+        return salida;
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main extends  ImplementacionInterface{
+    public static void main(String args[]){
+
+        System.out.println("Inicio");
+        ImplementacionInterface implementar = new ImplementacionInterface();
+        System.out.println(implementar.convertir("Hola mundo"));
+    }
+}
