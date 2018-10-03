0c608d163bc7e62fc248cfc10340c62606ca3671
Dann Joel <joelin_001@hotmail.com>
2018-10-03 10:26:39 -0500

    Se agregaron la solucion y la clase donde se implementara la solucion

---
+++ src/ImplementacionInterface.java
@@ -0,0 +1,7 @@
+import mx.com.sintelti.desafios.oporx.Solucion;
+
+public class ImplementacionInterface implements Solucion {
+    public String convertir(String entrada) {
+        return null;
+    }
+}
