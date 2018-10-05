0d5992f5b5a9b00fca9babda6963b4a0e508d750
Dann Joel <joelin_001@hotmail.com>
2018-10-05 13:07:21 -0500

    Se optimoso el codigo para hacerlo en 2 lineas.

--- src/ImplementacionInterface.java
+++ src/ImplementacionInterface.java
@@ -15,16 +15,13 @@ public class ImplementacionInterface implements Solucion {
         LimpiarValores();
         Integer acumulado=0;
         this.temp = entrada;
-
         ArrayList<String> Lista = new ArrayList<String>();//(Arrays.asList(this.entrada.split("")));
-
         for (int b =0; b<entrada.length(); b++){
-            this.temp = entrada.substring(b, entrada.length());
+            this.temp = entrada.substring(b);
             this.entrada = temp.substring(0,1);
             Lista.add(this.entrada);
-            //System.out.println(this.temp   + "      " + this.entrada);
+            System.out.println(this.temp   + "      " + this.entrada);
         }
-
         for (String caracter:  Lista) {
             if(caracter.equals("o")) {
                 caracter = "";
@@ -35,6 +32,9 @@ public class ImplementacionInterface implements Solucion {
             }
             this.salida += caracter;
         }
-        return salida;
+        String s= "oso";
+        for (String x=""; s.indexOf('o') > -1; s = s.substring(0,s.indexOf('o')) + (x=x+'x') + s.substring(s.indexOf('o')+1));
+        return s;
+        //return salida;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main extends  ImplementacionInterface{
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         //System.out.println("Inicio");
         ImplementacionInterface implementar = new ImplementacionInterface();
 
         System.out.println(implementar.convertir("Hola mundo"));
-        System.out.println(implementar.convertir("Como diciendo que no funciono"));
-        System.out.println(implementar.convertir("1o2o3o4o5o6o7o9o"));
-        System.out.println(implementar.convertir("1o2o"));
-        System.out.println(implementar.convertir("1o2o3o"));
+        //System.out.println(implementar.convertir("Como diciendo que no funciono"));
+        //System.out.println(implementar.convertir("1o2o3o4o5o6o7o9o"));
+        //System.out.println(implementar.convertir("1o2o"));
+        //System.out.println(implementar.convertir("1o2o3o"));
     }
 }
